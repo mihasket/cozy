b594ab944f085bcc7dbc5d7a169965282bfec69c
mihasket <miha.sket@gmail.com>
2026-02-17 19:13:58 +0100

    chore: semi rule

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script setup lang="ts">
-    console.log('This message is being logged by "App.vue", included via Vite')
+    console.log('This message is being logged by "App.vue", included via Vite');
 </script>
