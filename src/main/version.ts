5613bc3ed174a6cbc13b3f176b00b1c52ac52408
mihasket <miha.sket@gmail.com>
2026-02-18 16:48:08 +0100

    feat: semver version

---
+++ src/main/version.ts
@@ -0,0 +1,30 @@
+import { VersionError } from './mpd/error';
+
+export class Version {
+    private major: number;
+    private minor: number;
+    private patch: number;
+
+    constructor(major: number, minor: number, patch: number) {
+        if (major < 0 || minor < 0 || patch < 0) {
+            throw new VersionError('Version numbers must be non negative');
+        }
+
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+    }
+
+    static parse(semver: string): Version {
+        const match = semver.match(/^(\d+)\.(\d+)\.(\d+)$/);
+        if (!match) {
+            throw new VersionError('Invalid version');
+        }
+
+        return new Version(parseInt(match[1]), parseInt(match[2]), parseInt(match[3]));
+    }
+
+    toString(): string {
+        return `${this.major}.${this.minor}.${this.patch}`;
+    }
+}
