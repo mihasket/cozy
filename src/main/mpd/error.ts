933d301ccb90e93b433b50bdd15243c8e99e88a5
mihasket <miha.sket@gmail.com>
2026-02-18 16:39:48 +0100

    feat: simple mpd errors to start

---
+++ src/main/mpd/error.ts
@@ -0,0 +1,36 @@
+export class MpdError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace?.(this, this.constructor);
+    }
+}
+
+export class ResponseError extends MpdError {
+    constructor(message: string) {
+        super(message);
+        console.error(`MPD response error: ${message}`);
+    }
+}
+
+// Specific error types
+export class ParseError extends MpdError {
+    constructor(message: string) {
+        super(message);
+        console.error(`Parse error: ${message}`);
+    }
+}
+
+export class ClientError extends MpdError {
+    constructor(message: string) {
+        super(message);
+        console.error(`Client error: ${message}`);
+    }
+}
+
+export class VersionError extends MpdError {
+    constructor(message: string) {
+        super(message);
+        console.error(`Unsupported MPD version: ${message}`);
+    }
+}
